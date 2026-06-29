20c524fd810cf6852168cd37100f5e133c28cecc
qarrap_droid <sincostanrap@gmail.com>
2026-06-29 14:37:05 +0900

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -5,8 +5,8 @@
   onclick属性から呼ばれる関数があるため、現時点では module ではなく通常scriptとして読み込みます。
 */
 
-const APP_VERSION = "v1.23.4";
-const APP_VERSION_NOTE = "Buff削除ボタン表示修正";
+const APP_VERSION = "v1.23.5";
+const APP_VERSION_NOTE = "README実態反映";
 
 /* 種族係数。攻撃力係数と魔力係数は別管理。 */
 const RACE_COEFFS = {
